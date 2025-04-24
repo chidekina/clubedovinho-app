0ebfd009387902efcd8d20702310f62fb138e5b7
thiagoarruda09 <thiago.arruda.09@hotmail.com>
2025-04-24 20:20:27 -0300

    faz outra pagina de autor

--- src/pages/autores/Thiago/index.jsx
+++ src/pages/autores/Thiago/index.jsx
@@ -1,10 +1,10 @@
-import "./styles.scss";
 import * as React from 'react';
+import "./styles.scss";
 
 
 
 export default function Thiago() {
-  return <>
+  return (<>
 
     <div className="pages-thiago">
       
@@ -15,7 +15,8 @@ export default function Thiago() {
           <h1>Thiago arruda</h1>
           <h2>Desenvolvedor Fullstack</h2>
           <div className="spans">
-            <a href="www.linkedin.com/in/thiagoarruda09"><div>Mais sobre mim</div></a><a href="https://github.com/Thiagoarruda09?tab=repositories"><div>Portifólio</div></a>
+            <a target='_blank' href="https://www.linkedin.com/in/thiagoarruda09/"><div>Mais sobre mim</div></a>
+            <a target='_blank' href="https://github.com/Thiagoarruda09?tab=repositories"><div>Portifólio</div></a>
             </div>
         </div>
         <div className="col">
@@ -28,6 +29,6 @@ export default function Thiago() {
 
 
     </div>
-  </>
+  </>)
 
 }
